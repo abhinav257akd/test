4ac5b136e4885c8fee0ef2d2816175b69ae171cd
abhinav257akd <abhinav10oct@gmail.com>
2017-07-08 13:07:50 +0530

    Create searchopen.js

---
+++ searchopen.js
@@ -0,0 +1,15 @@
+//<![CDATA[
+
+  $(function () {
+    var search = $(".search");
+    var searchIcon = $("#search-icon");
+    var input = search.find("#search-input");
+
+    searchIcon.on("click", function () {
+      input.toggleClass("search-opened");
+      searchIcon.find(".fa").toggleClass("fa-close");
+      input.focus();
+    });
+  });
+
+// ]]>
